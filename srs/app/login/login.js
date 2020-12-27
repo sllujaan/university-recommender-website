ea7fcfcacd5a6dabf7dca8d1628c980515c5828e
SALMAN-ALTAF <SALMAN-ALTAF@SALMAN-ALTAF-PC>
2020-12-27 16:53:08 +0500

    added login components.

---
+++ srs/app/login/login.js
@@ -0,0 +1,10 @@
+import {
+    loadHeaderFooter
+} from "../util/util.js";
+
+var headerContainer = document.querySelectorAll(".header-container")[0];
+var footerContainer = document.querySelectorAll(".footer-container")[0];
+
+
+loadHeaderFooter(headerContainer, footerContainer);
+
