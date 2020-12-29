74208341007da8f8e5235107771ddbb463263d74
SALMAN-ALTAF <SALMAN-ALTAF@SALMAN-ALTAF-PC>
2020-12-29 15:13:12 +0500

    login bugs fixes.

--- srs/app/login/login.js
+++ srs/app/login/login.js
@@ -4,7 +4,25 @@ import {
 
 var headerContainer = document.querySelectorAll(".header-container")[0];
 var footerContainer = document.querySelectorAll(".footer-container")[0];
+var submitInput = document.querySelectorAll('#auth-submit')[0];
 
 
 loadHeaderFooter(headerContainer, footerContainer);
 
+
+
+document.forms[0].addEventListener("submit", e => {
+    e.preventDefault();
+
+    console.log("submit it..");
+    console.log(e.target);
+
+    //document.querySelectorAll('[type="submit"]')
+    console.log(submitInput);
+
+    submitInput.value = "Signing in ...";
+    submitInput.disabled = true;
+    submitInput.setAttribute("style", "background-color: grey;")
+
+
+})
