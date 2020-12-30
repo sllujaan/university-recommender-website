16b337f833addf52387adfeadbfa8d30b974200e
SALMAN-ALTAF <SALMAN-ALTAF@SALMAN-ALTAF-PC>
2020-12-30 15:36:08 +0500

    added login css

--- srs/app/login/login.js
+++ srs/app/login/login.js
@@ -5,6 +5,7 @@ import {
 var headerContainer = document.querySelectorAll(".header-container")[0];
 var footerContainer = document.querySelectorAll(".footer-container")[0];
 var submitInput = document.querySelectorAll('#auth-submit')[0];
+var errorText = document.querySelectorAll('.error-text')[0];
 
 
 loadHeaderFooter(headerContainer, footerContainer);
@@ -13,16 +14,37 @@ loadHeaderFooter(headerContainer, footerContainer);
 
 document.forms[0].addEventListener("submit", e => {
     e.preventDefault();
+    handleOnSubmit(e);
+    
+})
 
+
+const handleOnSubmit = (e) => {
     console.log("submit it..");
-    console.log(e.target);
 
-    //document.querySelectorAll('[type="submit"]')
-    console.log(submitInput);
+    disableInputs();
 
+    setTimeout(() => {
+        showAuthError();
+        EnableInputs();
+    }, 2000);
+}
+
+
+
+const showAuthError = () => {
+    errorText.innerText = "Invalid User Name or Password!";
+}
+
+const disableInputs = () => {
+    errorText.innerHTML = "&nbsp;";
     submitInput.value = "Signing in ...";
     submitInput.disabled = true;
-    submitInput.setAttribute("style", "background-color: grey;")
+    submitInput.setAttribute("style", "background-color: #d1d1d1; color: gray; cursor: default;");
+}
 
-
-})
+const EnableInputs = () => {
+    submitInput.value = "Sign in";
+    submitInput.disabled = false;
+    submitInput.setAttribute("style", "background-color: #5f0f4e; color: white; cursor: pointer;");
+}
