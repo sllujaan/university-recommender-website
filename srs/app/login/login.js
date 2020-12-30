cf251659fb647c4b4d29cae52c8b90f27b8e21f4
SALMAN-ALTAF <SALMAN-ALTAF@SALMAN-ALTAF-PC>
2020-12-30 16:20:25 +0500

    bugs fixes

--- srs/app/login/login.js
+++ srs/app/login/login.js
@@ -6,6 +6,11 @@ var headerContainer = document.querySelectorAll(".header-container")[0];
 var footerContainer = document.querySelectorAll(".footer-container")[0];
 var submitInput = document.querySelectorAll('#auth-submit')[0];
 var errorText = document.querySelectorAll('.error-text')[0];
+var inputs = document.querySelectorAll('input');
+console.log(inputs);
+
+
+
 
 
 loadHeaderFooter(headerContainer, footerContainer);
@@ -37,14 +42,40 @@ const showAuthError = () => {
 }
 
 const disableInputs = () => {
-    errorText.innerHTML = "&nbsp;";
-    submitInput.value = "Signing in ...";
-    submitInput.disabled = true;
-    submitInput.setAttribute("style", "background-color: #d1d1d1; color: gray; cursor: default;");
+
+
+    inputs.forEach(input => {
+
+        input.setAttribute("style", "background-color: #d1d1d1; color: gray; cursor: default;");
+        input.disabled = true;
+        if(input.getAttribute("type") === "submit") {
+            input.value = "Signing In...";
+            errorText.innerHTML = "&nbsp;";
+        }
+    
+    })
+
+    
+    // submitInput.value = "Signing in ...";
+    // submitInput.disabled = true;
+    // submitInput.setAttribute("style", "background-color: #d1d1d1; color: gray; cursor: default;");
 }
 
 const EnableInputs = () => {
-    submitInput.value = "Sign in";
-    submitInput.disabled = false;
-    submitInput.setAttribute("style", "background-color: #5f0f4e; color: white; cursor: pointer;");
+
+
+    inputs.forEach(input => {
+
+        input.setAttribute("style", "background-color: white; color: black;");
+        input.disabled = false;
+        if(input.getAttribute("type") === "submit") {
+            input.setAttribute("style", "cursor: pointer;");
+            input.value = "Sign In";
+        }
+    
+    })
+
+    // submitInput.value = "Sign in";
+    // submitInput.disabled = false;
+    // submitInput.setAttribute("style", "background-color: #5f0f4e; color: white; cursor: pointer;");
 }
