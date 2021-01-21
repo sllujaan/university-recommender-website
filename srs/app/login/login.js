bbbf7283d0f744c4e2013e8430ab7f180d75dbd3
SALMAN-ALTAF <SALMAN-ALTAF@SALMAN-ALTAF-PC>
2021-01-21 17:05:12 +0500

    login bugs fixes.

--- srs/app/login/login.js
+++ srs/app/login/login.js
@@ -26,34 +26,41 @@ document.forms[0].addEventListener("submit", e => {
 
 const handleOnSubmit = (e) => {
     console.log("submit it..");
+    console.log(e.target);
 
-    disableInputs();
+    disableInputs(e);
 
     setTimeout(() => {
-        showAuthError();
-        EnableInputs();
+        showAuthError(e);
+        EnableInputs(e);
     }, 2000);
 }
 
 
 
-const showAuthError = () => {
+const showAuthError = (e) => {
     errorText.innerText = "Invalid User Name or Password!";
 }
 
-const disableInputs = () => {
+const disableInputs = (e) => {
 
 
-    inputs.forEach(input => {
+    // inputs.forEach(input => {
 
-        input.setAttribute("style", "background-color: #d1d1d1; color: gray; cursor: default;");
-        input.disabled = true;
-        if(input.getAttribute("type") === "submit") {
-            input.value = "Signing In...";
-            errorText.innerHTML = "&nbsp;";
-        }
+    //     input.setAttribute("style", "background-color: #d1d1d1; color: gray; cursor: default;");
+    //     input.disabled = true;
+    //     if(input.getAttribute("type") === "submit") {
+    //         input.value = "Signing In...";
+    //         errorText.innerHTML = "&nbsp;";
+    //     }
     
-    })
+    // })
+
+
+    e.target.setAttribute("style", "pointer-events: none;");
+    submitInput.setAttribute("style", "background-color: #d1d1d1; color: gray; cursor: default;");
+    submitInput.value = "Signing In...";
+    errorText.innerHTML = "&nbsp;";
 
     
     // submitInput.value = "Signing in ...";
@@ -61,19 +68,23 @@ const disableInputs = () => {
     // submitInput.setAttribute("style", "background-color: #d1d1d1; color: gray; cursor: default;");
 }
 
-const EnableInputs = () => {
+const EnableInputs = (e) => {
 
 
-    inputs.forEach(input => {
+    // inputs.forEach(input => {
 
-        input.setAttribute("style", "background-color: white; color: black;");
-        input.disabled = false;
-        if(input.getAttribute("type") === "submit") {
-            input.setAttribute("style", "cursor: pointer;");
-            input.value = "Sign In";
-        }
+    //     input.setAttribute("style", "background-color: white; color: black;");
+    //     input.disabled = false;
+    //     if(input.getAttribute("type") === "submit") {
+    //         input.setAttribute("style", "cursor: pointer;");
+    //         input.value = "Sign In";
+    //     }
     
-    })
+    // })
+
+    e.target.setAttribute("style", "pointer-events: all;");
+    submitInput.setAttribute("style", "background-color: #5f0f4e; color: white; cursor: pointer;");
+    submitInput.value = "Sign in";
 
     // submitInput.value = "Sign in";
     // submitInput.disabled = false;
