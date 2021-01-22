c9dc23747761f075af0dcbf1af53f9d25f05a56a
SALMAN-ALTAF <SALMAN-ALTAF@SALMAN-ALTAF-PC>
2021-01-22 16:21:21 +0500

    bugs fixes.

--- srs/app/header/header.js
+++ srs/app/header/header.js
@@ -2,4 +2,17 @@
 
 
 
+var barResponsive = document.getElementById("bar-responsive");
+var headerSidebar = document.getElementById("header-sidebar");
 
+console.log(barResponsive);
+
+
+barResponsive.addEventListener("click", e => {
+    headerSidebar.setAttribute("style", "right: 0");
+})
+
+
+// document.addEventListener("DOMContentLoaded", e => {
+//     barResponsive.click();
+// })
