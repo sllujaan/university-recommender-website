d1bb2bf6402c3db52439bfd3c9f05c3ed4ec9e1c
SALMAN-ALTAF <SALMAN-ALTAF@SALMAN-ALTAF-PC>
2021-01-24 12:07:11 +0500

    bugs fixes.

--- srs/app/register/register.js
+++ srs/app/register/register.js
@@ -3,12 +3,13 @@ import { URL_REGISTER_NAME, URL_REGISTER_EMAIL } from "../urls/urlResolver.js";
 
 
 
-
 var headerContainer = document.querySelectorAll(".header-container-wrapper")[0];
 var footerContainer = document.querySelectorAll(".footer-container-wrapper")[0];
 var submitInput = document.querySelectorAll('#auth-submit')[0];
 var errorText = document.querySelectorAll('.error-text')[0];
 var inputs = document.querySelectorAll('input');
+loadHeaderFooter(headerContainer, footerContainer);
+
 
 var name = inputs[0];
 var email = inputs[1];
@@ -55,75 +56,7 @@ const validateFieldDB = async (e, url) => {
         displayServerError();
         console.error(err);
     })
-
 }
-
-
-
-
-
-const nameIsValidDB = async (e) => {
-    const reqeustData = "name=" + e.target.value;
-    console.log(reqeustData);
-
-    fetch(URL_REGISTER_NAME, {
-        method: 'POST', // *GET, POST, PUT, DELETE, etc.
-        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        redirect: 'follow', // manual, *follow, error
-        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        body: reqeustData // body data type must match "Content-Type" header
-    })
-    .then(res => {
-
-        switch (res.status) {
-            case 409:
-                displayConflictError(document.getElementById("name-error"), e.target.value, "Name")
-                //displayNameConflictError(e);
-                break;
-            case 200:
-                displayInputOK(document.getElementById("name-error"), e.target.value);
-                //displayNameOK(e);
-                break;
-            default:
-                displayServerError();
-                break;
-        }
-    })
-    .catch(err => {
-        displayServerError();
-        console.error(err);
-    })
-
-}
-
-
-
-
-// fetch("http://localhost/university-recommender-website-server-api/register/name.php", {
-//     method: 'POST', // *GET, POST, PUT, DELETE, etc.
-//     cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-//     headers: {
-//         'Content-Type': 'application/x-www-form-urlencoded'
-//     },
-//     body: "name=admin" // body data type must match "Content-Type" header
-// })
-// .then(res => res.text())
-// .then(text => {
-//     console.log(text);
-// })
-// .catch(err => {
-//     console.error(err);
-// })
-
-
-
-
-
-
-
 
 
 
@@ -141,20 +74,6 @@ const displayInputOK = (errElement, text) => {
 }
 
 
-
-
-const displayNameConflictError = (e) => {
-    var errElement = document.getElementById("name-error");
-    errElement.classList.remove("success-text");
-    errElement.classList.add("error-text");
-    errElement.innerHTML = "Name &quot;" + e.target.value + "&quot; already exists";
-}
-
-const displayEmailConflictError = (e) => {
-    var errElement = document.getElementById("email-error");
-    errElement.innerHTML = "Email &quot;" + e.target.value + "&quot; already exists";
-}
-
 const displayServerError = () => {
     var errElements = document.querySelectorAll(".error-text");
     errElements.forEach(errElment => {
@@ -166,23 +85,6 @@ const displayServerError = () => {
 }
 
 
-
-const displayNameOK = (e) => {
-    var errElement = document.getElementById("name-error");
-    errElement.classList.remove("error-text");
-    errElement.classList.add("success-text");
-    errElement.innerHTML = e.target.value + "&nbsp;&#10004;";
-}
-
-
-
-
-
-loadHeaderFooter(headerContainer, footerContainer);
-
-// setTimeout(() => {
-//     loadHeaderJS(document);
-// }, 2000);
 
 document.forms[0].addEventListener("submit", e => {
     e.preventDefault();
@@ -205,61 +107,15 @@ const handleOnSubmit = (e) => {
 
 
 
-const showAuthError = (e) => {
-    errorText.innerText = "Invalid User Name or Password!";
-}
-
-
 const disableInputs = (e) => {
-
-
-    // inputs.forEach(input => {
-
-    //     input.setAttribute("style", "background-color: #d1d1d1; color: gray; cursor: default;");
-    //     input.disabled = true;
-    //     if(input.getAttribute("type") === "submit") {
-    //         input.value = "Signing In...";
-    //         errorText.innerHTML = "&nbsp;";
-    //     }
-    
-    // })
-
-
     e.target.setAttribute("style", "pointer-events: none;");
     submitInput.setAttribute("style", "background-color: #d1d1d1; color: gray; cursor: default;");
     submitInput.value = "Signing In...";
     errorText.innerHTML = "&nbsp;";
-
-    
-    // submitInput.value = "Signing in ...";
-    // submitInput.disabled = true;
-    // submitInput.setAttribute("style", "background-color: #d1d1d1; color: gray; cursor: default;");
 }
 
 const EnableInputs = (e) => {
-
-
-    // inputs.forEach(input => {
-
-    //     input.setAttribute("style", "background-color: white; color: black;");
-    //     input.disabled = false;
-    //     if(input.getAttribute("type") === "submit") {
-    //         input.setAttribute("style", "cursor: pointer;");
-    //         input.value = "Sign In";
-    //     }
-    
-    // })
-
     e.target.setAttribute("style", "pointer-events: all;");
     submitInput.setAttribute("style", "background-color: #5f0f4e; color: white; cursor: pointer;");
     submitInput.value = "Sign in";
-
-    // submitInput.value = "Sign in";
-    // submitInput.disabled = false;
-    // submitInput.setAttribute("style", "background-color: #5f0f4e; color: white; cursor: pointer;");
-}
-
-
-const disableElement = (elment) => {
-    elment.setAttribute("style", "background-color: #d1d1d1; color: gray; cursor: default;");
 }
--- srs/app/urls/urlResolver.js
+++ srs/app/urls/urlResolver.js
@@ -4,3 +4,6 @@ export const URL_BASE = "http://localhost/university-recommender-website-server-
 
 export const URL_REGISTER_NAME = URL_BASE + "register/name.php";
 export const URL_REGISTER_EMAIL = URL_BASE + "register/email.php";
+export const URL_PROGRAM = URL_BASE + "util/program.php";
+export const URL_COUNTRY = URL_BASE + "util/country.php";
+export const URL_CITY = URL_BASE + "util/city.php";
