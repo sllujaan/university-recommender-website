a5fb0d48f70ae8fea606fce4d28b71a400066579
SALMAN-ALTAF <SALMAN-ALTAF@SALMAN-ALTAF-PC>
2021-01-25 11:04:26 +0500

    bugs fixes

--- srs/app/user/user.js
+++ srs/app/user/user.js
@@ -4,7 +4,23 @@ import { URL_USER_LOGIN } from "../urls/urlResolver.js";
 
 var headerContainer = document.querySelectorAll(".header-container-wrapper")[0];
 var footerContainer = document.querySelectorAll(".footer-container-wrapper")[0];
+var userRows = document.querySelectorAll(".user-row");
+var searchUser = document.getElementById("search-user");
+
 
 loadHeaderFooter(headerContainer, footerContainer);
 
 
+
+
+const searchTable = (value) => {
+    console.log(value);
+}
+
+
+
+searchUser.addEventListener("keyup", e => {
+    console.log(userRows);
+    userRows[0].setProperty("style", "display: none;");
+    searchTable(e.target.value);
+})
