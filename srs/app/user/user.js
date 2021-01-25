1bbd85fc7c615a62ade9f056f0835748fea54f1a
SALMAN-ALTAF <SALMAN-ALTAF@SALMAN-ALTAF-PC>
2021-01-25 11:37:29 +0500

    bugs fixes.

--- srs/app/user/user.js
+++ srs/app/user/user.js
@@ -13,14 +13,75 @@ loadHeaderFooter(headerContainer, footerContainer);
 
 
 
-const searchTable = (value) => {
-    console.log(value);
+const searchTable = (searchValue) => {
+    userRows.forEach(userRow => {
+        const userName = userRow.getElementsByClassName("user-name")[0].innerText;
+        // console.log(userName);
+        // console.log(userName.includes(searchValue));
+        if(!userName.includes(searchValue)) {
+            hideElement(userRow);
+        }
+        else {
+            showElement(userRow);
+        }
+    })
+
 }
+
+const hideElement = (element) => {
+    element.setAttribute("style", "display: none;");
+}
+
+const showElement = (element) => {
+    element.setAttribute("style", "display: table-row;");
+}
+
+
+const users = [
+    {name: "jake"},
+    {name: "mike"},
+]
+
+const initUsersInTable = (users) => {
+    users.forEach(user => {
+
+    })
+}
+
+const generateUserRow = (user) => {
+    var tr = document.createElement("tr");
+    var tdName = document.createElement("td");
+    var tdEmail = document.createElement("td");
+    var tdCountry = document.createElement("td");
+    var tdCity = document.createElement("td");
+    var tdProgram = document.createElement("td");
+
+    tr.setAttribute("class", "user-row");
+    tdName.setAttribute("class", "user-name");
+    tdName = user.name;
+    tdEmail = user.email;
+    tdCountry = user.country;
+    tdCity = user.city;
+    tdProgram = user.program;
+
+    tr.append(tdName);
+    tr.append(tdEmail);
+    tr.append(tdCountry);
+    tr.append(tdCity);
+    tr.append(tdProgram);
+    
+}
+
+<tr class="user-row">
+                    <td class="user-name">jake</td>
+                    <td>name</td>
+                    <td>name</td>
+                    <td>name</td>
+                    <td>name</td>
+                </tr>
 
 
 
 searchUser.addEventListener("keyup", e => {
-    console.log(userRows);
-    userRows[0].setProperty("style", "display: none;");
     searchTable(e.target.value);
 })
