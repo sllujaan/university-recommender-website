6c56bbf5e0168af34f8e87d8e225055c7e231cf9
SALMAN-ALTAF <SALMAN-ALTAF@SALMAN-ALTAF-PC>
2021-01-26 16:29:04 +0500

    bugs fixes.

--- srs/app/header/header.js
+++ srs/app/header/header.js
@@ -1,16 +1,19 @@
 
 
-
 export function loadHeaderJS(DOM) {
+    var body = DOM.getElementsByTagName("body")[0];
     var barResponsive = DOM.getElementById("bar-responsive");
     var headerSidebar = DOM.getElementById("header-sidebar");
     var closeBar = DOM.getElementById("close-bar");
 
     barResponsive.addEventListener("click", e => {
+        console.log(body);
+        body.setAttribute("style", "position: fixed; overflow: hidden;");
         headerSidebar.setAttribute("style", "right: 0");
     })
     
     closeBar.addEventListener("click", e => {
+        body.setAttribute("style", "position: relative; overflow: auto;");
         headerSidebar.setAttribute("style", "right: 100%");
     })
     
@@ -18,6 +21,9 @@ export function loadHeaderJS(DOM) {
     DOM.addEventListener("DOMContentLoaded", e => {
         barResponsive.click();
     })
+
+    
+
 }
 
 
