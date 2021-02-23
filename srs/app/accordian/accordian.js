606c21c070812593924532c75e08020be7edb2fe
SALMAN-ALTAF <SALMAN-ALTAF@SALMAN-ALTAF-PC>
2021-02-23 16:11:46 +0500

    bugs fixes.

--- srs/app/accordian/accordian.js
+++ srs/app/accordian/accordian.js
@@ -6,9 +6,15 @@ var openedprogramPanelEl = null;
 document.addEventListener("click", e => {
 
     const isProgramAccordian = e.target.classList.contains("accordian-program-item");
-    
+    const isAccordianTab = e.target.classList.contains("tablinks");
+
+    console.log(e.target);
     if(isProgramAccordian) {
         toggleAccordian(e.target.parentElement);
+    }
+
+    if(isAccordianTab) {
+        openTabDetails(e, e.target.innerText);
     }
 
 
@@ -43,4 +49,20 @@ const toggleCaretIcon = (iconEl, up) => {
         iconEl.classList.remove("fa-caret-up");
         iconEl.classList.add("fa-caret-down");
     }
+}
+
+
+
+function openTabDetails(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.target.classList.add("active");
 }
