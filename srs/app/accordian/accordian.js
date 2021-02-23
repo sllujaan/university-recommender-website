3f937b1fdd7dd2e372304812ae78be51348217bb
SALMAN-ALTAF <SALMAN-ALTAF@SALMAN-ALTAF-PC>
2021-02-23 15:57:52 +0500

    adding accordians data

--- srs/app/accordian/accordian.js
+++ srs/app/accordian/accordian.js
@@ -1,6 +1,6 @@
 
 
-var OPENED_PANEL;
+var openedprogramPanelEl = null;
 
 
 document.addEventListener("click", e => {
@@ -15,10 +15,32 @@ document.addEventListener("click", e => {
 })
 
 const toggleAccordian = (accordianEl) => {
+    //document.querySelectorAll(".caret-accordian")[0];
     var panel = accordianEl.nextElementSibling;
+    var caretIcon = accordianEl.querySelectorAll(".caret-accordian")[0];
+    console.log(caretIcon);
     if (panel.style.display === "block") {
         panel.style.display = "none";
+        toggleCaretIcon(caretIcon, false);
     } else {
         panel.style.display = "block";
+        toggleCaretIcon(caretIcon, true);
+
+    }
+}
+
+
+const hideOpenedPanel = (elment) => {
+    elment.style.display = "none";
+}
+
+const toggleCaretIcon = (iconEl, up) => {
+    if(up) {
+        iconEl.classList.remove("fa-caret-down");
+        iconEl.classList.add("fa-caret-up");
+    }
+    else {
+        iconEl.classList.remove("fa-caret-up");
+        iconEl.classList.add("fa-caret-down");
     }
 }
